c266d738c8bf8caf4029f9c1bc4b891954f62458
Keith Bryant <kabryant@cinci.rr.com>
2017-07-14 11:22:09 -0400

    Listen

--- nodegame.js
+++ nodegame.js
@@ -14,7 +14,7 @@ console.log(words.length);
 app.get('/', function(req, res) {
   const gameWord = words[Math.floor((Math.random() * 100000) + 1)];
   console.log(gameWord);
-  res.send('nodegame is the name of this file, but a small text string is all that I have been able to reproduce on heroku. Sad.');
+  res.send('Oh, so putting a couple of words on a blank ass screen is supposed to be impressive?? I did it myself, and even I am not impressed. If I had a powerful app to brag about, I would feel very different I am sure. But I do not. So I do not.');
 });
 // return gameWord
 //must connect "./views" ***Must create "views" folder and insert mustache file, which goes to res.render//
